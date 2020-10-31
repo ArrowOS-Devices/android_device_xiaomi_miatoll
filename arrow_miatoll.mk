06b019573fabff57d6b22a654b14ba26de3b5573
Sonal Singh <sonal.singh.19993@gmail.com>
2020-10-31 18:41:05 +0530

    miatoll: lineage -> arrow
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ arrow_miatoll.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2020 The ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier
+PRODUCT_NAME := arrow_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6250
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer
+DEVICE_MAINTAINER := SonalSingh18
