2a07349ce4c12dfae99a456acd9c29e7916717ae
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-03-07 00:40:55 +0530

    Update and rename arrow_miatoll.mk to lineage_miatoll.mk

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
@@ -23,4 +23,4 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Maintainer
-DEVICE_MAINTAINER := SonalSingh18
+DEVICE_MAINTAINER := SwapnilVicky
